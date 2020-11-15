898d4b9c491db45200cf592265a895712b3cd3b9
Cristian Vega <97cvega@gmail.com>
2020-11-15 16:49:57 +0100

    ManagerIOMock refactor

--- src/test/java/mock/ManagerIOMock.java
+++ src/test/java/mock/ManagerIOMock.java
@@ -4,60 +4,52 @@ import interfaces.IManagerIO;
 
 public class ManagerIOMock implements IManagerIO {
 
-
-
 	int[] arrayInput;
 	int[] arrayInputRandom;
-	int position=0;
-	int positionRandom=0;
-	
+	int position = 0;
+	int positionRandom = 0;
 
+	public ManagerIOMock(int[] inputsSequence, int[] randomSequence) {
+		arrayInput = new int[inputsSequence.length];
+		arrayInput = inputsSequence;
+		arrayInputRandom = new int[randomSequence.length];
+		arrayInputRandom = randomSequence;
+	}
 
-	public ManagerIOMock(int[] inputsSequence , int[] randomSequence){
-		arrayInput = new int[inputsSequence.length] ;
-		arrayInput= inputsSequence;
-		arrayInputRandom = new int[randomSequence.length] ;
-		arrayInputRandom= randomSequence;
-	}
-	
-	
-	public ManagerIOMock(int[] inputsSequence ){
+	public ManagerIOMock(int[] inputsSequence) {
 
-		arrayInput = new int[inputsSequence.length] ;
-		arrayInput= inputsSequence;
-	
+		arrayInput = new int[inputsSequence.length];
+		arrayInput = inputsSequence;
+
 	}
-	
-	public ManagerIOMock (){
-		
+
+	public ManagerIOMock() {
+
 	}
-	
+
 	@Override
 	public int inInt() {
-		
-		if(position<arrayInput.length) {
-			int aux= arrayInput[position];
+
+		if (position < arrayInput.length) {
+			int aux = arrayInput[position];
 			this.position++;
 			return aux;
 		}
 		return 1;
-		
-				 
+
 	}
 
 	@Override
 	public int getRandomInt() {
-		
-		if(positionRandom<arrayInputRandom.length) {
-			int aux= arrayInputRandom[positionRandom];
+
+		if (positionRandom < arrayInputRandom.length) {
+			int aux = arrayInputRandom[positionRandom];
 			this.positionRandom++;
 			return aux;
 		}
 		return 1;
-		
+
 	}
-
-
 
 	@Override
 	public void out(String data) {
