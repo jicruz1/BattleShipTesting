2599248fc9a87e98eca8cdf6f0e590a44da720b7
Cristian Vega <97cvega@gmail.com>
2020-11-15 16:38:10 +0100

    Doc comments ManagerIO

--- src/main/java/BattelshipTesting/ManagerIO.java
+++ src/main/java/BattelshipTesting/ManagerIO.java
@@ -7,6 +7,7 @@ import interfaces.IManagerIO;
 
 /**
  * Class to manager Input/Outputs
+ * 
  * @author Cristian Vega
  *
  */
@@ -18,23 +19,38 @@ public class ManagerIO implements IManagerIO {
 
 	}
 
-	public int inInt() {
-		return scanner.nextInt();
-	}
-
+	/**
+	 * Function that prints a String in the output buffer
+	 */
 	public void out(String data) {
 		System.out.print(data);
 	}
 
+	/**
+	 * Function that prints an Integer in the output buffer
+	 */
 	public void out(int data) {
 		System.out.print(data);
 	}
 
+	/**
+	 * Function reads keyboard input from an Integer value
+	 */
+	public int inInt() {
+		return scanner.nextInt();
+	}
+
+	/**
+	 * Function that generates a random number between 0-10
+	 */
 	public int getRandomInt() {
 		rn = new Random();
 		return rn.nextInt(10) + 1;
 	}
 
+	/**
+	 * Function that generates random orientation between 0-4
+	 */
 	public int getRandomOrientacion() {
 		rn = new Random();
 		return rn.nextInt(4) + 0;
