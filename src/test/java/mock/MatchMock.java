26fb2458c0d5a466129427096f9cc502845ab8c5
RogerPiera <65972120+RogerPiera@users.noreply.github.com>
2020-12-16 19:09:04 +0100

    Updated MatchMock Class

--- src/test/java/mock/MatchMock.java
+++ src/test/java/mock/MatchMock.java
@@ -11,6 +11,15 @@ public class MatchMock extends Match {
 		
 		this.user = new PlayerGameLoopMock(Constants.NAME_PLAYER, managerIO);
 		this.randomIA = new Player(Constants.RANDOM_PLAYER_IA, managerIO);
+	}
+	
+	public void startGame()
+	{
 		this.startMatch();
+	}
+	
+	public boolean hasWon()
+	{
+		return this.endGame;
 	}
 }
